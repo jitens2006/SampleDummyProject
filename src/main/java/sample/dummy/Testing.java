b439163821af24a676691485f30f94371b721ad6
Jitender <jitens2006@gmail.com>
2024-07-12 16:45:39 +0530

    Test Commit

---
+++ src/main/java/sample/dummy/Testing.java
@@ -0,0 +1,14 @@
+package sample.dummy;
+
+import oracle.ucp.jdbc.PoolDataSource;
+import oracle.ucp.jdbc.PoolDataSourceFactory;
+public class Testing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		PoolDataSource dataSource = PoolDataSourceFactory.getPoolDataSource();
+		System.out.println("Testingffffzxz     sssssssssssssxzxxzxzxzxzxf");
+		System.out.println("Testingffffzxz    sssssssssssssxzxxzxzxzxzxf");
+		System.out.println("Testingxccxcxc    xcxi             ngsdsds");
+	}
+}
