b4972558fac47e2533e79f432726c9788d03528f
Jitender <jitens2006@gmail.com>
2024-10-19 21:17:30 +0530

    Checking for revert Testing

--- src/main/java/sample/dummy/Testing.java
+++ src/main/java/sample/dummy/Testing.java
@@ -5,7 +5,7 @@ import oracle.ucp.jdbc.PoolDataSourceFactory;
 public class Testing {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
 		PoolDataSource dataSource = PoolDataSourceFactory.getPoolDataSource();
 		System.out.println("Testingffffzxz     sssssssssssssxzxxzxzxzxzxf");
 	}
