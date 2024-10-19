f4c87de628db46cf1690d80b2160f111c8565fbd
Jitender <jitens2006@gmail.com>
2024-10-20 00:24:49 +0530

    Deleted unnecessary files

--- src/main/java/sample/dummy/Testing.java
+++ src/main/java/sample/dummy/Testing.java
@@ -7,8 +7,5 @@ public class Testing {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		PoolDataSource dataSource = PoolDataSourceFactory.getPoolDataSource();
-		System.out.println("Testingffffzxz     sssssssssssssxzxxzxzxzxzxf");
-		System.out.println("Testingffffzxz    sssssssssssssxzxxzxzxzxzxf");
-		System.out.println("Testingxccxcxc    xcxi             ngsdsds");
 	}
 }
