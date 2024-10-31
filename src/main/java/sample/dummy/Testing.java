c188542d09ea774cf33a7f27acb828563ae8da1a
jitens2006 <39851409+jitens2006@users.noreply.github.com>
2024-11-01 01:00:07 +0530

    Gitgub: NewLine added dup removed

--- src/main/java/sample/dummy/Testing.java
+++ src/main/java/sample/dummy/Testing.java
@@ -10,6 +10,6 @@ public class Testing {
 		System.out.println("Testingffffzxz     sssssssssssssxzxxzxzxzxzxf");
 		System.out.println("Testingffffzxz    sssssssssssssxzxxzxzxzxzxf");
 		System.out.println("Testingxccxcxc    xcxi             ngsdsds");
-		System.out.println("New Line added New Line Added");
+		System.out.println("New Line added");
 	}
 }
